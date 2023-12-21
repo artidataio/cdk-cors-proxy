87aa81698600c1296679f326550ed59addca5a7d
Imaduddin Haetami <imaduddin.htm@gmail.com>
2023-12-22 00:11:36 +0700

    First Deploy.

--- lib/cdk-cors-proxy-stack.ts
+++ lib/cdk-cors-proxy-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class CdkCorsProxyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaProxy = new lambda.Function(this, "indexHandler", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCorsProxyQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new apigw.LambdaRestApi(this, "CdkCorsProxy", {
+      handler: lambdaProxy,
+    });
   }
 }
